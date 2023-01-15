8f2d284b679011564b452d3fa94fe9ce19b53fac
ben <ybc125@naver.com>
2023-01-16 01:48:12 +0900

    [feat] 전체 채팅 구현(#4)

--- src/main/java/com/yonggamsa/withsuyeonjung/chat/framework/adapters/input/websocket/ChatAdapter.java
+++ src/main/java/com/yonggamsa/withsuyeonjung/chat/framework/adapters/input/websocket/ChatAdapter.java
@@ -2,6 +2,7 @@ package com.yonggamsa.withsuyeonjung.chat.framework.adapters.input.websocket;
 
 import com.yonggamsa.withsuyeonjung.chat.framework.adapters.input.websocket.request.Chat;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
@@ -18,10 +19,10 @@ public class ChatAdapter {
     //Client가 SEND할 수 있는 경로
     //stompConfig에서 설정한 applicationDestinationPrefixes와 @MessageMapping 경로가 병합됨
     //"/pub/chat/enter"
-    @MessageMapping("/chat/message")
-    public void sendMessage(Chat chat) {
+    @MessageMapping("/chat/room/{chatroomId}/message")
+    public void sendMessage(@DestinationVariable String chatroomId, Chat chat) {
+        chat.setChatroomId(chatroomId);
         chat.setCreatedDate(ZonedDateTime.now(ZoneId.of("Asia/Seoul")));
-        chat.setSender();
         simpleMessagingTemplate.convertAndSend("/topic/chat/room/" + chat.getChatroomId(), chat);
     }
 }
