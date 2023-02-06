75c530a522b0d4306ee35de38b8c9e74c9248932
gettls <gettls@daum.net>
2023-02-06 21:16:20 +0900

    [fix] merge develop code

--- src/main/java/com/yonggamsa/withsuyeonjung/chat/framework/adapters/output/repository/ChatManagementRepository.java
+++ src/main/java/com/yonggamsa/withsuyeonjung/chat/framework/adapters/output/repository/ChatManagementRepository.java
@@ -1,8 +1,1 @@
 package com.yonggamsa.withsuyeonjung.chat.framework.adapters.output.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-
-public class ChatManagementRepository {
-}
